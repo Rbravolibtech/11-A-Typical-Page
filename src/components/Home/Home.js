9b3fc0130ba694e0e3dee9cafc2f49da09775695
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-09-20 11:18:28 -0600

    styling for log out button and making context dynamic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Button from "../UI/Button/Button";
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
 
@@ -7,7 +7,9 @@ const Home = (props) => {
 	return (
 		<Card className={classes.home}>
 			<h1>Welcome back!</h1>
-			<button onClick={props.onLogout}>Sign Out</button>
+			<Button className={classes.button} onClick={props.onLogout}>
+				Sign Out
+			</Button>
 		</Card>
 	);
 };
